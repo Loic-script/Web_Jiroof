a8c40f5b13ff70714a790bd65756826e63b739c0
Loic-script <razafimamonjyfanevaloic@gmail.com>
2025-06-04 14:39:22 +0300

    interface statique

--- login.php
+++ login.php
@@ -113,7 +113,7 @@ if($_SERVER["REQUEST_METHOD"] === "POST" &&
             $_SESSION["fname"] = $user["fname"];
             $_SESSION["age"] = $user["age"];
             $_SESSION["photo"] = $user["photo"];
-            header("Location: Jiroof.html");
+            header("Location: index.html");
             exit();
         }
         else{
