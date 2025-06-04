0396875d25384c24fa52ae90a951f6dadb9be852
MarcoRazafy <hei.marco.10@gmail.com>
2025-06-04 10:52:23 +0300

    mise en place de la structure

---
+++ Option.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: { primary: "#D4AF37", secondary: "#FFFFFF" },
+        borderRadius: {
+          none: "0px",
+          sm: "4px",
+          DEFAULT: "8px",
+          md: "12px",
+          lg: "16px",
+          xl: "20px",
+          "2xl": "24px",
+          "3xl": "32px",
+          full: "9999px",
+          button: "8px",
+        },
+      },
+    },
+  };
---
+++ Script.js
@@ -0,0 +1,83 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.querySelector(".ri-menu-line").parentElement;
+    const nav = document.querySelector("nav");
+
+    menuButton.addEventListener("click", function () {
+      if (nav.classList.contains("hidden")) {
+        nav.classList.remove("hidden");
+        nav.classList.add(
+          "flex",
+          "flex-col",
+          "absolute",
+          "top-16",
+          "right-4",
+          "bg-black",
+          "border",
+          "border-gray-800",
+          "p-4",
+          "rounded",
+          "shadow-lg",
+          "z-50",
+          "space-y-4",
+        );
+      } else {
+        nav.classList.add("hidden");
+        nav.classList.remove(
+          "flex",
+          "flex-col",
+          "absolute",
+          "top-16",
+          "right-4",
+          "bg-black",
+          "border",
+          "border-gray-800",
+          "p-4",
+          "rounded",
+          "shadow-lg",
+          "z-50",
+          "space-y-4",
+        );
+      }
+    });
+  });
+
+//   cherche function
+document.addEventListener("DOMContentLoaded", function () {
+    const searchInput = document.querySelector(".search-input");
+
+    searchInput.addEventListener("focus", function () {
+      this.classList.add("border-primary");
+    });
+
+    searchInput.addEventListener("blur", function () {
+      if (this.value === "") {
+        this.classList.remove("border-primary");
+      }
+    });
+  });
+
+//   
+document.addEventListener("DOMContentLoaded", function () {
+    const newsletterInput = document.querySelector(".newsletter-input");
+    const subscribeButton = newsletterInput.nextElementSibling;
+
+    newsletterInput.addEventListener("focus", function () {
+      this.classList.add("border-primary");
+    });
+
+    newsletterInput.addEventListener("blur", function () {
+      if (this.value === "") {
+        this.classList.remove("border-primary");
+      }
+    });
+
+    subscribeButton.addEventListener("click", function () {
+      const email = newsletterInput.value;
+      if (email && /^\S+@\S+\.\S+$/.test(email)) {
+        alert("Merci de vous être inscrit à notre newsletter !");
+        newsletterInput.value = "";
+      } else {
+        alert("Veuillez entrer une adresse email valide.");
+      }
+    });
+  });
